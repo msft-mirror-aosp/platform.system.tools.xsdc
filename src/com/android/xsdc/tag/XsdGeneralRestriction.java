6d912c8fe8aaf5030175e2bbf6a91bae328c2cfc
Sundong Ahn <sundongahn@google.com>
2020-01-06 18:15:25 +0900

    Support AttributeGroup tag
    The attributeGroup tag is supported in xsdc. When creating the class,
    all attributes in the attributeGroup are included to class rather than
    creating additional class or interface.
    
    Test: m -j && xsdc-cpp-tests
    Bug: 147109860
    Change-Id: I4dc72a4eeb0381b76d4d5b187a45fb4c28233e3f

--- src/com/android/xsdc/tag/XsdGeneralRestriction.java
+++ src/com/android/xsdc/tag/XsdGeneralRestriction.java
@@ -22,13 +22,16 @@ import java.util.List;
 public class XsdGeneralRestriction extends XsdTag {
     final private XsdType base;
     final private List<XsdAttribute> attributes;
+    final private List<XsdAttributeGroup> attributeGroups;
     final private List<XsdElement> elements;
 
     public XsdGeneralRestriction(XsdType base, List<XsdAttribute> attributes,
+            List<XsdAttributeGroup> attributeGroups,
             List<XsdElement> elements) {
         super(null, null);
         this.base = base;
         this.attributes = Collections.unmodifiableList(attributes);
+        this.attributeGroups = Collections.unmodifiableList(attributeGroups);
         this.elements = Collections.unmodifiableList(elements);
     }
 
@@ -38,6 +41,10 @@ public class XsdGeneralRestriction extends XsdTag {
 
     public List<XsdAttribute> getAttributes() {
         return attributes;
+    }
+
+    public List<XsdAttributeGroup> getAttributeGroups() {
+        return attributeGroups;
     }
 
     public List<XsdElement> getElements() {
